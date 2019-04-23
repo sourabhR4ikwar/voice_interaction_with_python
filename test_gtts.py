699b4d2d70dfc56d58a09f75f8e3fea2756c0157
sourabhR4ikwar <sourabh.d.raikwar@gmail.com>
2019-04-23 14:32:51 +0530

    Fixed Issue with multiple files

--- test_gtts.py
+++ test_gtts.py
@@ -1,11 +1,21 @@
 import os
 from playsound import playsound
-from gtts import gTTS as speak
+from gtts import gTTS
 
-text = "hey sourabh";
 
-voice = speak(text=text,lang="en",slow=False)
-voice.save("sample.mp3")
+def speak(text, count):
+    voice = gTTS(text=text,lang="hi",slow=False)
+    voice.save("tempText2speech"+str(count)+".mp3")
+    playsound("tempText2speech"+str(count)+".mp3")
+    os.remove("tempText2speech"+str(count)+".mp3")
 
-#os.system("mpg321 sample.mp3")
-playsound("sample.mp3")
+
+text = ""
+count = 0
+while text != "bye":
+    print("Write something you want me to say.")
+    print(" >> ",end="")
+    text = input();
+    print("Processing Text...")
+    speak(text, count)
+    count = count + 1
